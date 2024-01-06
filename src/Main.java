6e1921d649f4104e6697da837b2ad6c9e082ea26
JulioGaekel <juliogaekel@gmail.com>
2024-01-06 12:13:24 +0100

    Add programming exercises chp.1
    Several syntax and logic exercises, mostly math operations.

---
+++ src/Main.java
@@ -0,0 +1,61 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // DISPLAY THREE MESSAGES
+        /*System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");*/
+
+        // DISPLAY FIVE MESSAGES
+        /*System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");*/
+
+        // DISPLAY A PATTERN
+        /*System.out.println("     J     A     V     V     A");
+        System.out.println("     J    A A     V   V     A A");
+        System.out.println(" J   J   AAAAA     V V     AAAAA");
+        System.out.println("  J J   A     A     V     A     A");*/
+
+        // PRINT A TABLE
+
+
+        // COMPUTE EXPRESSIONS
+       /* System.out.println((9.5 * 4.5) - (2.5 * 3.0) / (45.5 - 3.5));*/
+
+        // SUMMATION OF A SERIES
+        /*System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 +9);*/
+
+        // APPROXIMATE PI
+        /*System.out.println("Pi = " + 4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) - (1.0/11.0)));
+        System.out.println("Pi = " + 4.0 * (1.0 - (1.0/3.0) + (1.0/5.0) - (1.0/7.0) + (1.0/9.0) - (1.0/11.0) + (1.0/13.0)));*/
+
+        // AREA AND PERIMETER OF A CIRCLE
+        /*double radius = 5.5;
+        double pi = 3.14159265;
+
+        System.out.println("Perimeter = " + (2 * radius * pi));
+        System.out.println("Area = " + ((radius * radius) * pi));*/
+
+        // AREA AND PERIMETER OF A RECTANGLE
+        /*double width = 4.5;
+        double height = 7.9;
+
+        System.out.println("Area = " + (width + height));*/
+
+        // 1.10 AVERAGE SPEED IN MILES
+        double distanceKilometers = 14;
+        double distanceMiles = distanceKilometers /1.6;
+        double timeInSeconds = (45 * 60) + 30;
+        double timeInMinutes = timeInSeconds / 60;
+        double timeHours = timeInMinutes / 60;
+
+        System.out.println(timeInMinutes);
+        System.out.println(timeHours);
+        System.out.println("The average speed of the runner is " + Math.round((distanceMiles / timeHours)) + " miles per hour.");
+
+
+    }
+}
