a79698dd86e3e8c2c36efd1be9834aaa1e6588b4
JulioGaekel <juliogaekel@gmail.com>
2024-01-15 14:45:58 +0100

    Create ComputeAreaWithConsoleInput.java

---
+++ src/ComputeAreaWithConsoleInput.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.Scanner; // Scanner is in the java.util package
+public class ComputeAreaWithConsoleInput {
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter a radius
+        System.out.print("Enter a number for radius: ");
+        double radius = sc.nextDouble();
+
+        // Compute area
+        double area = radius * radius * 3.14159;
+
+        // Display results
+        System.out.println("The area of the circle with a radius of " + radius + " is " + area);
+    }
+}
