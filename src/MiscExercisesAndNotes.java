0f4088a045ca4701d5a5e2d5aeb692ad10979651
JulioGaekel <juliogaekel@gmail.com>
2024-01-16 13:57:39 +0100

    Add Integer Literals

---
+++ src/MiscExercisesAndNotes.java
@@ -0,0 +1,12 @@
+public class MiscExercisesAndNotes {
+    public static void main(String[] args) {
+
+
+        /* // Integer Literals
+        System.out.println(0B1111); // To denote a binary integer literal, use leading 0b or 0B (zero B). Displays 15.
+        System.out.println(07777); // To denote an octal integer literal, use a leading 0 (zero). Displays 4095.
+        System.out.println(0XFFFF); // To denote a hexadecimal integer literal, use a leading 0X (zero X). Displays 65535.*/
+
+
+    }
+}
