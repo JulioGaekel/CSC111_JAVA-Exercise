0bf615f0e4cfd3a43339356163b9c22349c4fae9
JulioGaekel <juliogaekel@gmail.com>
2024-01-16 09:43:02 +0100

    Add Scanner, user prompt and average compute

--- src/ComputeAverage.java
+++ src/ComputeAverage.java
@@ -1,4 +1,21 @@
 import java.util.Scanner;
 public class ComputeAverage {
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
 
+        // Prompt user for input
+        System.out.print("Enter three numbers: ");
+
+        // Create variables to store user inputs
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        double num3 = sc.nextDouble();
+
+        // Compute average
+        double average = (num1 + num2 + num3)/3;
+
+        // Display result
+        System.out.println("The average of " + num1 + ", " + num2 + " and " + num3 + " is " + average);
+    }
 }
