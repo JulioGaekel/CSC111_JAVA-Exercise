d23b583533d9fcd0ad17457eacb6598b3b1fd6b4
JulioGaekel <juliogaekel@gmail.com>
2024-01-16 14:06:53 +0100

    Add scientific notation explanation

--- src/MiscExercisesAndNotes.java
+++ src/MiscExercisesAndNotes.java
@@ -12,6 +12,7 @@ public class MiscExercisesAndNotes {
         System.out.println(07777); // To denote an octal integer literal, use a leading 0 (zero). Displays 4095.
         System.out.println(0XFFFF); // To denote a hexadecimal integer literal, use a leading 0X (zero X). Displays 65535.*/
 
-
+        /* // Scientific Notation
+        Floating-point literals can be written in scientific notation in the form of a * 10b. For example, the scientific notation for 123.456 is 1.23456 * 102 and for 0.0123456 is 1.23456 * 10-2. A special syntax is used to write scientific notation numbers. For example, 1.23456 * 102 is written as 1.23456E2 or 1.23456E+2 and 1.23456 * 10-2 as 1.23456E-2. E (or e) represents an exponent and can be in either lowercase or uppercase.*/
     }
 }
