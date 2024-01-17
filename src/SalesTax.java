b883d144795d1611682df7b21f1455e3cd44c05f
JulioGaekel <juliogaekel@gmail.com>
2024-01-17 13:29:14 +0100

    Create SaleTax.java

---
+++ src/SalesTax.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SalesTax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter purchase amount: ");
+        double purchaseAmount = sc.nextDouble();
+
+        double tax = purchaseAmount * 0.06;
+        System.out.println("Sales tax is $" + (int)(tax * 100) / 100.0);
+
+    }
+}
