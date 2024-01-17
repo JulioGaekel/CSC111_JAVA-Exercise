6e71c80cbedfa0489b5cf870384af5263611e0b6
JulioGaekel <juliogaekel@gmail.com>
2024-01-17 09:04:07 +0100

    Add increment and decrement operators

--- src/MiscExercisesAndNotes.java
+++ src/MiscExercisesAndNotes.java
@@ -14,5 +14,28 @@ public class MiscExercisesAndNotes {
 
         /* // Scientific Notation
         Floating-point literals can be written in scientific notation in the form of a * 10b. For example, the scientific notation for 123.456 is 1.23456 * 102 and for 0.0123456 is 1.23456 * 10-2. A special syntax is used to write scientific notation numbers. For example, 1.23456 * 102 is written as 1.23456E2 or 1.23456E+2 and 1.23456 * 10-2 as 1.23456E-2. E (or e) represents an exponent and can be in either lowercase or uppercase.*/
+
+        // 2.1 Evaluating Expressions and Operator Precedence
+
+/*
+        // Augmented Assignment Operators
+        double a = 6.5;
+        a += a + 1;
+        System.out.println(a);
+        a = 6;
+        a /= 2;
+        System.out.println(a);
+*/
+/*      INCREMENT AND DECREMENT OPERATORS
+        int a = 6;
+        int b = a++; // Two parts: 1) b = a 2) then modify a by adding 1.
+        System.out.println(a); // Prints 7
+        System.out.println(b); // Prints
+
+        a = 6;
+        b = ++a; // b equals a after being modified by 1. The a variable will also change permanently. Difference with the previous exercise is the order in which b is assign the value, pre- or post-modification.
+        System.out.println(a);
+        System.out.println(b); */
+
     }
 }
