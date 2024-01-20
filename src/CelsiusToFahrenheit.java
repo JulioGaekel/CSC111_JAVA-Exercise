db1a733ceaef30e744ed6450ad3e86e32c685b9f
JulioGaekel <juliogaekel@gmail.com>
2024-01-20 18:24:05 +0100

    Create CelsiusToFahrenheit.java

---
+++ src/CelsiusToFahrenheit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user for degrees in Celsius
+        System.out.print("Enter degrees in Celsius: ");
+        double celsiusDegrees = sc.nextDouble();
+
+        // Compute Celsius to Fahrenheit
+        double fahrenheitDegrees = (9.0 / 5.0) * celsiusDegrees + 32.0;
+
+        // Display result
+        System.out.println(celsiusDegrees + " degrees Celsius are " + fahrenheitDegrees + " degrees Fahrenheit");
+    }
+}
