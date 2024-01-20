6541267e09a50c98ec3d8ed4427d4cb8a70dbf33
JulioGaekel <juliogaekel@gmail.com>
2024-01-20 18:35:09 +0100

    Create ComputeVolumeCylinder,java

---
+++ src/ComputeVolumeCylinder.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ComputeVolumeCylinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter a radius: ");
+        double radius = sc.nextDouble();
+        final double PI = 3.14159;
+
+        System.out.print("Enter a lenght: ");
+        double length = sc.nextDouble();
+
+        // Compute
+        double area = radius * radius * PI;
+        double volume = area * length;
+
+        // Display results
+        System.out.println("The area is " + area);
+        System.out.println("The volume is " + volume);
+    }
+}
