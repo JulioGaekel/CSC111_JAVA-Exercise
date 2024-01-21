ae5aed730cdeb8cf855bdf2b2b5e7d45ee356cb9
JulioGaekel <juliogaekel@gmail.com>
2024-01-21 09:25:41 +0100

    Create ConvertFeetToMeters.java

---
+++ src/ConvertFeetToMeters.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ConvertFeetToMeters {
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter a value for feet: ");
+        double feet = sc.nextDouble();
+
+        // Process conversion
+        double meters = feet * 0.305;
+
+        // Display results
+        System.out.println(feet + " feet is " + meters + " meters");
+    }
+}
