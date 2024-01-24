97c8243817e3aaa7d6915574fedf5ffe4f9231d7
JulioGaekel <juliogaekel@gmail.com>
2024-01-24 09:48:54 +0100

    Style: Change initial prompt message

--- src/Chapter3_Selections/ComputeTaxUS.java
+++ src/Chapter3_Selections/ComputeTaxUS.java
@@ -9,7 +9,7 @@ public class ComputeTaxUS {
         Scanner sc = new Scanner(System.in).useLocale(Locale.ENGLISH);
 
         // Prompt user to enter filing status
-        System.out.print("(0-single filer, 1-married jointly or " + "qualifying widow(er), 2-married separately, 3-head of " + "household) Enter the filing status: ");
+        System.out.print("(0-single filer, 1-married jointly or qualifying widow(er), 2-married separately, 3-head of household) Enter the filing status: ");
         int status = sc.nextInt();
 
         // Prompt user to enter taxable income
