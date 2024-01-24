31a5e5cd4343aa02b6b380eec31a2a714977708c
JulioGaekel <juliogaekel@gmail.com>
2024-01-24 18:24:35 +0100

    Create Lottery.java

---
+++ src/Chapter3_Selections/Lottery.java
@@ -0,0 +1,30 @@
+package Chapter3_Selections;
+
+import java.util.Scanner;
+
+public class Lottery {
+    public static void main(String[] args) {
+        // Generate random lottery number
+        int lottery = (int)(Math.random() * 100);
+
+        // Prompt the user to enter a guess
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter lottery number (two digits): ");
+        int guess = sc.nextInt();
+
+        // Get digits from lottery
+        int lotteryDigit1 = lottery / 10;
+        int lotteryDigit2 = lottery % 10;
+
+        // Get digits from guess
+        int guessDigit1 = guess / 10;
+        int guessDigit2 = guess % 10;
+
+        System.out.println("The lottery number is " + lottery);
+
+        // Check guess
+        if (guess == lottery) {
+            System.out.println("Exact guess: you win $10,000");
+        } else if () // CONTINUAR ACA
+    }
+}
