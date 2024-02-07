35f8ba7ee5c18206b872178e112acd500916c292
JulioGaekel <juliogaekel@gmail.com>
2024-02-07 19:22:07 +0100

    Create SubtractionQuizLoop.java

---
+++ src/Chapter4/SubtractionQuizLoop.java
@@ -0,0 +1,49 @@
+package Chapter4;
+
+import java.util.Scanner;
+
+public class SubtractionQuizLoop {
+    public static void main(String[] args) {
+        // Create Scanner object
+        Scanner sc = new Scanner(System.in);
+
+        // Create variables
+        final int NUMBER_OF_QUESTIONS = 5;
+        int correctCount = 0; // Count the number of correct answers
+        int count = 0; // Count the number of questions
+        long startTime = System.currentTimeMillis();
+        String output = " "; // output string is initially empty
+
+        // Generate two random single-digit integers. This should repeat until NUMBER_OF_QUESTIONS
+        while (count < NUMBER_OF_QUESTIONS) {
+            int number1 = (int) (Math.random() * 10);
+            int number2 = (int) (Math.random() * 10);
+
+            // 2. If number1 < number2, swap number1 with number2
+            if (number1 < number2) {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            // Prompt the student to answer the math problem
+            System.out.print("What is " + number1 + " - " + number2 + "? ");
+            int answer = sc.nextInt();
+
+            // Grade the answer and display the result
+            if (number1 - number2 == answer) {
+                System.out.println("You are correct!");
+                correctCount++;
+            } else {
+                System.out.println("Your answer is wrong.\n" + number1 + " - " + number2 + " should be " + (number1 - number2));
+            }
+
+            count++;
+            output += "\n" + number1 + "-" + number2 + "=" + answer + ((number1 - number2 == answer) ? " correct" : " wrong");
+        }
+        long endTime = System.currentTimeMillis();
+        long testTime = endTime - startTime;
+
+        System.out.println("Correct count is " + correctCount + "\nTest time is " + testTime / 1000 + " seconds\n" + output);
+    }
+}
